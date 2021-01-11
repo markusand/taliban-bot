981a18dfcb666e25d4e46487391c031e2f6df2c4
markusand <mvilella@obsa.ad>
2021-01-12 00:53:00 +0100

    Add reactions to basic messages
    [+] Greetings messages like "Hola!", "Bon dia" or "boneees!"
    [+] Single emoji messages
    [+] Laugh messages, including variants hahaha, jijiji...
    [+] Conformity messages like "ok" or "valeee"
    [+] Mentions to taliban

--- api/events.js
+++ api/events.js
@@ -1,4 +1,43 @@
 import SlackBot from '../slack-bot';
 
+const regex = {
+	GREETING: /^(ho+la+!*\s*)+$|^(Bo+n dia+!*\s*)+$|^(e+i+\s*!*)+$|^(bo+ne+s!*)$/i,
+	EMOJIS: /^\s*(:[^:\s]+:\s*)+$/,
+	LAUGH: /^((h|j)+(a|e|i)+(j|h)*\s*){2,}!*$/i,
+	CONFORMITY: /^(o+k(?:e+y+)*!*|va+le+!*\s*)+$/i,
+};
+
 export default new SlackBot(bot => {
+	bot.message(regex.GREETING, async ({ user }) => {
+		await bot.whisper(`No ens perdem en formalismes, <@${user}>. Directe al gra!`);
+	});
+
+	bot.message(regex.EMOJIS, async ({ text }) => {
+		await bot.whisper('Les reaccions a missatges es van inventar per no haver de fer servir els emojis així.\nInclus jo sé fer-ho!');
+		const emojis = text.split(':').filter(part => part.trim());
+		await bot.react(emojis);
+	});
+
+	bot.message(regex.LAUGH, async () => {
+		await bot.whisper("Jo no tinc sentit de l'humor, però si he de riure faig servir les reaccions. Tu també hauries.");
+		await bot.react(['laughing', 'rolling_on_the_floor_laughing', 'joy', 'sweat_smile']);
+	});
+
+	bot.message(regex.CONFORMITY, async ({ text }) => {
+		await bot.whisper(`Per acabar dient _${text}_ millor utilitza una reacció.`);
+		await bot.react(['ok', 'ok_hand', 'thumbsup', 'top']);
+	});
+
+	bot.message('taliban', async ({ user }) => {
+		await bot.respond(`És bo saber que sóc tant popular, <@${user}>. _Allahu akbar_!`);
+	});
+
+	bot.mention(async ({ user }) => {
+		const { SECTION, IMAGE } = bot.blocks;
+		await bot.respond([
+			SECTION(`Infidel <@${user}>, pronunciant el nom del profeta en va!`),
+			IMAGE('https://i.gifer.com/76jQ.gif'),
+			SECTION('Oh espera... això era _Mahoma_, no jo. Perdó.'),
+		]);
+	});
 });
